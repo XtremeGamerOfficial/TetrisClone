bfb8b657fd82e626ac0901957ed30cd60af6aab4
XtremePrime <super_madalin2006@yahoo.com>
2013-02-27 08:35:11 +0200

    Meh
    Unimportant update

--- src/World.java
+++ src/World.java
@@ -1,6 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Random;
+
 
 public class World {
 
-	private int[][] blocks;
+	private Rectangle[] tiles = new Rectangle[500];
+	Random rand = new Random();
+	
+	public void draw(Graphics g){
+        for(int i = 0; i < tiles.length; ++i){
+        	g.setColor(new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)));
+            g.fillRect(tiles[i].x, tiles[i].y, tiles[i].width, tiles[i].height);
+        }
+	}
 	
 }
